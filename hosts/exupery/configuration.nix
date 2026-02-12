9156f3fe69cca7480e68b7b3cf982e5889a7337e
Herzaristide <aristide.pichereau@gmail.com>
2026-02-12 18:05:59 +0100

    Allow user 'aristide' to have an empty password for passwordless SSH access in exupery host configuration.

--- hosts/exupery/configuration.nix
+++ hosts/exupery/configuration.nix
@@ -42,4 +42,10 @@
       PermitRootLogin = "no";
     };
   };
+
+  # Allow user to have empty password for passwordless SSH access
+  users.users.aristide = {
+    hashedPassword = null;  # Allow empty password
+    password = null;         # No password set
+  };
 }
