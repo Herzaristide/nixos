b84978e48f14a7c65ae6609331825a666d849699
Herzaristide <aristide.pichereau@gmail.com>
2026-05-15 00:11:48 +0200

    refactor: reorganize module imports and enhance session variables in home configuration

--- home/home.nix
+++ home/home.nix
@@ -7,17 +7,40 @@
   ...
 }:
 
-{
-  imports = [
+let
+  headlessModules = [
     ./modules/network
     ./modules/shell
     ./modules/code
     ./modules/code/vscode/vscode-server.nix
-  ]
-  ++ (if head then [ ./head.nix ] else [ ]);
+  ];
+
+  headfullModules = [
+    ./head.nix
+    ./modules/hyprland.nix
+    ./modules/code/vscode/vscode.nix
+    ./modules/wezterm.nix
+    ../quickshell/quickshell.nix
+    ./modules/walker.nix
+    ./modules/accent/accent.nix
+    ./modules/kde.nix
+    ./modules/chromium.nix
+  ];
+in
+{
+  imports = headlessModules ++ (if head then headfullModules else [ ]);
 
   # Home Manager settings
   home.username = "aristide";
   home.homeDirectory = "/home/aristide";
   home.stateVersion = "25.11";
+
+  home.sessionVariables = {
+    EDITOR = "micro";
+    VISUAL = "micro";
+    PAGER = "less";
+    MANPAGER = "less";
+    BROWSER = "chromium";
+    TERMINAL = "wezterm";
+  };
 }
