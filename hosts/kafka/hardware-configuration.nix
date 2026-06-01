5443a089bccbae1b2afdd8c26cc823402338ec74
Herzaristide <aristide.pichereau@gmail.com>
2026-06-01 22:56:05 +0200

    feat: remove initrd-ssh module and update configurations for LUKS unlocking

--- hosts/kafka/hardware-configuration.nix
+++ hosts/kafka/hardware-configuration.nix
@@ -28,8 +28,6 @@
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
-  swapDevices = [ ];
-
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
